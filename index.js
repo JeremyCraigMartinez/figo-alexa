312a323ff84085bff5f161dbcbb639ceeabf38bb
JeremyCraigMartinez <jeremymartinez11@gmail.com>
2016-10-10 18:10:03 +0200

    hello world example from tutorial

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
-var alexa = require('alexa-app');
-
-// Allow this module to be reloaded by hotswap when changed
+'use strict';
 module.change_code = 1;
+var _ = require('lodash');
+var Alexa = require('alexa-app');
+var app = new Alexa.app('airportinfo');
+var FAADataHelper = require('./faa_data_helper');
 
-// Define an alexa-app
-var app = new alexa.app('pizza');
-app.launch(function(req,res) {
-    res.say("Hello World!!");
+app.launch(function(req, res) {
+  var prompt = 'For delay information, tell me an Airport code.';
+  res.say(prompt).reprompt(prompt).shouldEndSession(false);
 });
 
-app.intent('pizza', {
-        "slots":{"PIZZA":"LITERAL"},
-        "utterances":["{Get me|I want to order} {diabolo|margarita|PIZZA} "]
-    }, function(req, res) {
-        //get the slot
-        var pizzaType = req.slot('PIZZA');
-        var reprompt = 'Tell me which pizza you want once more, you mumbling bastard.';
-        if (!pizzaType) {
-            var prompt = 'Tell me pizza you want.';
-            res.say(prompt).reprompt(reprompt).shouldEndSession(false);
-            return true;
-        } else {
-            // faking getting pizza
-            if (pizzaType === 'diabolo' || pizzaType === 'margarita') {
-                res.say('Ok, you\'ll get your ' + pizzaType + '.');
-            } else {
-                var unknownPizzaPrompt = 'I do not know this pizza.';
-                res.say(unknownPizzaPrompt).reprompt(reprompt).shouldEndSession(false);
-                res.shouldEndSession(false);
-            }
-        }
+app.intent('airportinfo', {
+  'slots': {
+    'AIRPORTCODE': 'FAACODES'
+  },
+  'utterances': ['{|flight|airport} {|delay|status} {|info} {|for} {-|AIRPORTCODE}']
+},
+  function(req, res) {
+    //get the slot
+    var airportCode = req.slot('AIRPORTCODE');
+    var reprompt = 'Tell me an airport code to get delay information.';
+if (_.isEmpty(airportCode)) {
+      var prompt = 'I didn\'t hear an airport code. Tell me an airport code.';
+      res.say(prompt).reprompt(reprompt).shouldEndSession(false);
+      return true;
+    } else {
+      var faaHelper = new FAADataHelper();
+
+faaHelper.requestAirportStatus(airportCode).then(function(airportStatus) {
+        console.log(airportStatus);
+        res.say(faaHelper.formatAirportStatus(airportStatus)).send();
+      }).catch(function(err) {
+        console.log(err.statusCode);
+        var prompt = 'I didn\'t have data for an airport code of ' + airportCode;
+        res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
+      });
+      return false;
     }
+  }
 );
 
 //hack to support custom utterances in utterance expansion string
 var utterancesMethod = app.utterances;
 app.utterances = function() {
-    return utterancesMethod().replace(/\{\-\|/g, '{');
+return utterancesMethod().replace(/\{\-\|/g, '{');
 };
 
 module.exports = app;
