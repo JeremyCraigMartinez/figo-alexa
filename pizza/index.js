867ce821ace15024485d46f4bc34f13bb6f98aed
Max Tkachenko <mt@figo.io>
2016-10-11 13:31:51 +0200

    fix session ending after pizza selection

--- pizza/index.js
+++ pizza/index.js
@@ -17,7 +17,7 @@ module.exports = function(app) {
             } else {
                 // faking getting pizza
                 if (pizzaType === 'diabolo' || pizzaType === 'margherita' || pizzaType === 'salami') {
-                    res.say('Ok, you\'ll get your ' + pizzaType + '.');
+                    res.say('Ok, you\'ll get your ' + pizzaType + '. How do you want to pay?').shouldEndSession(false);
                 } else {
                     var unknownPizzaPrompt = 'I do not know this pizza.';
                     res.say(unknownPizzaPrompt).reprompt(reprompt).shouldEndSession(false);
