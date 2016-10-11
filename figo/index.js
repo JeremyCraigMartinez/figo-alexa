16e9f9ab1757e9a14c309c0b1053ca92d3644389
JeremyCraigMartinez <jeremymartinez11@gmail.com>
2016-10-11 11:35:53 +0200

    fixing up balance stuff, cleaning up some dialog

--- figo/index.js
+++ figo/index.js
@@ -8,9 +8,11 @@ var FigoHelper = new FigoHelper();
 var util = require('./util');
 
 // selected account for pizza payment
-var accounts = null;
-var selectedAccount = null;
+var accounts;
+var selectedAccount;
 var standingOrders;
+var paycheckDate;
+var balance;
 
 // general variables
 var samsungCost = 720;
@@ -46,16 +48,20 @@ module.exports = function(app) {
                 FigoHelper.listAccounts().then(function(accts) {
                     accounts = accts
                     selectedAccount = util.getAccount(accts, 'type', paymentType);
+                    balance = selectedAccount.balance.balance
+
+                    // subtract some of the balance to make dialog work for bank loan suggestion
+                    balance = Math.ceil(balance - 1900);
 
                     // check that you have the funds
-                    if (app.locals.pizzaOrder && app.locals.pizzaOrder.cost > selectedAccount.balance.balance) {
+                    if (app.locals.pizzaOrder && app.locals.pizzaOrder.cost > balance) {
                         prompt = 'You are too poor to afford this pizza.';
                         reprompt = 'Choose a different account.';
                         res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
                     } else {
                         console.log(JSON.stringify(accts));
                         console.log(JSON.stringify(selectedAccount));
-                        res.say('ok').send();
+                        res.say('ok, your balance is ' + balance + ' euro').send();
                     }
                 });
                 return false;
@@ -66,7 +72,7 @@ module.exports = function(app) {
     app.intent('getBalance', {
             'utterances':['{what is|what\'s} my balance?']
         }, function(req, res) {
-            var prompt = 'Your balance is ' + selectedAccount.balance.balance;
+            var prompt = 'Your balance is ' + balance;
             res.say(prompt).send();
         }
     );
@@ -119,7 +125,7 @@ module.exports = function(app) {
                     t = (so.standingOrderDetails.timeUnit === 'M') ? 'month' : t;
                     t = (so.standingOrderDetails.timeUnit === 'W') ? 'week' : t;
                     t = (so.standingOrderDetails.timeUnit === 'Y') ? 'year' : t;
-                    var prompt = 'Yes, you have 1. You owe ' +
+                    var prompt = 'Yes, you have 1. You owe a tremendous ' +
                                   so.orderAmount + ' euro to ' +
                                   so.payeeAcct.holderName +
                                   ' on day ' + so.standingOrderDetails.execDay +
@@ -136,8 +142,8 @@ module.exports = function(app) {
         }, function(req, res) {
             var prompt;
             var so = standingOrders[0];
-            if (selectedAccount.balance.balance - so.orderAmount - samsungCost > 0) {
-                prompt = 'Yes, after your purchase and payment to ' + so.payeeAcct.holderName + ' you will have ' + (selectedAccount.balance.balance - so.orderAmount - samsungCost) + ' in your account.';
+            if (balance - so.orderAmount - samsungCost > 0) {
+                prompt = 'Yes, after your purchase and payment to ' + so.payeeAcct.holderName + ' you will have ' + (balance - so.orderAmount - samsungCost) + ' in your account.';
             } else {
                 prompt = 'No, you will overdraw your account after your upcoming payments due.';
             }
@@ -150,11 +156,19 @@ module.exports = function(app) {
         }, function(req, res) {
             var prompt = 'Based on recent account history, your paycheck should come on the ';
             FigoHelper.transactions().then(function(transactions) {
-                var date = util.predictPaycheck(transactions);
-                prompt += date + 'th.';
+                paycheckDate = util.predictPaycheck(transactions);
+                prompt += paycheckDate + 'th.';
                 res.say(prompt).send();
             })
             return false;
+        }
+    );
+
+    app.intent('summary', {
+            'utterances':['{|so} {|can you} give me a summary {|of everything|of everything so far}?']
+        }, function(req, res) {
+            var prompt = 'The Samsung Galaxy Note costs ' + samsungCost + ' Euro and your Balance is ' + balance + ' Euro. You have  outgoing payments planed of ' + standingOrders[0].orderAmount + ' Euro which all together would leave you with ' + (balance-samsungCost-standingOrders[0].orderAmount) + ' and your next paycheck should come on the ' + paycheckDate + 'th.';
+            res.say(prompt).send();
         }
     );
 
